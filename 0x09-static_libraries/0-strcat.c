d9e48aac042b68dc7d47bc53901d97a738e064e2
addoray1 <richardbampoe@gmail.com>
2023-10-30 18:11:05 -0700

    update

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,30 +1,21 @@
 #include "main.h"
- /**
+/**
  * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
- * Return: void	
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i = 0;
+	int j = 0;
 
-
-	i = 0;
 	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
+	i++;
 	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}		
-
-
+	dest[i] = src[j];
+	i++;
+	j++;
 	dest[i] = '\0';
 	return (dest);
 }
--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,21 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string to the standard output, followed by a new line.
+ * @str: The string to be printed.
+ */
 void _puts(char *str)
-#include<stdio.h>
+{
+printf("%s\n", str);
+}
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+_puts("\"Programming is like building a multilingual puzzle\"");
+return (0);
 }
